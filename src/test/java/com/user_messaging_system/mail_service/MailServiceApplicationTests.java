186bc0b6e780f60112d750cd3adb4d408ff5e050
burakyapici747 <burakyapici747@gmail.com>
2024-09-28 13:25:15 +0300

    project init

---
+++ src/test/java/com/user_messaging_system/mail_service/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_messaging_system.mail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
